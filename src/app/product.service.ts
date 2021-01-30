5d77d51585e83ac639939e28a9966b4fe42a4bee
Ozgur TEZEL <ozgur.tezel@christie.nhs.uk>
2021-01-30 01:38:23 +0000

    6.17 - displaying products list

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
+import { Http } from '@angular/http';
 import { Album } from './album';
 import { Observable } from 'rxjs';
 import {Product} from './product';
