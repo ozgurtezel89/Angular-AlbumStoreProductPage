d8b1079547a4759f0daba405b83405de3e7c84ec
Ozgur TEZEL <ozgur.tezel@christie.nhs.uk>
2021-01-31 01:29:04 +0000

    7 routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,21 @@ import { ProductTracklistingComponent } from './product-tracklisting/product-tra
 import { ProductListComponent } from './product-list/product-list.component';
 import { RouterModule, Routes } from '@angular/router';
 
-const appRoutes: Routes = [];
+const appRoutes: Routes = [
+  {
+    path: "products",
+    component: ProductListComponent
+  },
+  {
+    path: "product/:id",
+    component: ProductPageComponent
+  },
+  {
+    path: "",
+    redirectTo: "products",
+    pathMatch: "full"
+  }
+];
 
 @NgModule({
   declarations: [
@@ -24,7 +38,8 @@ const appRoutes: Routes = [];
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [
     ProductService
